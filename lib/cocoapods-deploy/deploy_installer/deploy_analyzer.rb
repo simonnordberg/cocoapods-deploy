9d95e6375ef3e6dee866d6e450fab6ad77228378
James Campbell <james@supmenow.com>
2016-03-04 16:57:45 +0000

    Fixes mixins.

--- lib/cocoapods-deploy/deploy_installer/deploy_analyzer.rb
+++ lib/cocoapods-deploy/deploy_installer/deploy_analyzer.rb
@@ -1,6 +1,10 @@
 module Pod
   class Installer
     class DeployAnalyzer < Analyzer
+
+      include Config::Mixin
+      include InstallationOptions::Mixin
+
       def sources
           []
       end
