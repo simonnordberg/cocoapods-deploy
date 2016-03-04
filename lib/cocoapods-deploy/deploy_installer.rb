44fa2b2fa1554d504909a782e5f31c2ad83169c1
James Campbell <james@supmenow.com>
2016-03-04 16:56:43 +0000

    Super.

--- lib/cocoapods-deploy/deploy_installer.rb
+++ lib/cocoapods-deploy/deploy_installer.rb
@@ -4,12 +4,8 @@ module Pod
   class DeployInstaller < Installer
 
     def create_analyzer
-
-      # Workaround for fact we can't get access to `installation_options`
-      original_analyzer = super
-
       DeployAnalyzer.new(sandbox, podfile, lockfile).tap do |analyzer|
-        analyzer.installation_options = original_analyzer.installation_options
+        analyzer.installation_options = super.installation_options
       end
     end
   end
