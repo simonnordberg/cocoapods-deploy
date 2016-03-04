9ecabd3a5b22bbac681bb30d76aa5197f1410f49
James Campbell <james@supmenow.com>
2016-03-04 16:46:38 +0000

    Adds mixins.

--- lib/cocoapods-deploy/deploy_installer.rb
+++ lib/cocoapods-deploy/deploy_installer.rb
@@ -1,5 +1,9 @@
 module Pod
   class DeployInstaller < Installer
+
+    include Config::Mixin
+    include InstallationOptions::Mixin
+
     def create_analyzer
       DeployAnalyzer.new(sandbox, podfile, lockfile).tap do |analyzer|
         analyzer.installation_options = installation_options
