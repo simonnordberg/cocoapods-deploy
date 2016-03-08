ab31f6315a59a397f4fcf87a9906792fc41d2094
James Campbell <james@supmenow.com>
2016-03-08 20:51:12 +0000

    Preparing for release.

--- spec/command/deploy_spec.rb
+++ spec/command/deploy_spec.rb
@@ -4,9 +4,8 @@ module Pod
   describe Command::Deployment do
     describe 'CLAide' do
       it 'registers it self' do
-        Command.parse(%w{ deployment }).should.be.instance_of Command::Deployment
+        Command.parse(%w{ deploy }).should.be.instance_of Command::Deployment
       end
     end
   end
 end
-
