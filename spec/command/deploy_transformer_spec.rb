f5df87e766b27971d4b240227dd72324486085bb
James Campbell <james@supmenow.com>
2016-03-10 17:16:48 +0000

    Preparing to update the original podfile transformation tests for the specification tests.

--- spec/command/deploy_transformer_spec.rb
+++ spec/command/deploy_transformer_spec.rb
@@ -67,6 +67,69 @@ module Pod
     end
 
     describe "when transforming specification" do
+      # it "should preserve external dependencies" do
+      #   lockfile = Lockfile.new({})
+      #   original_podfile = Podfile.new do |p|
+      #     p.pod "Polly", :git => "http://example.org"
+      #   end
+      #
+      #   podfile = transform_podfile(lockfile, original_podfile)
+      #   dependency = Dependency.new("Polly", {:git => "http://example.org"})
+      #   podfile.dependencies.should.include dependency
+      # end
+      #
+      # describe "when transforming repo dependencies" do
+      #   it "should abort when absent from lockfile" do
+      #     lockfile = Lockfile.new({})
+      #     original_podfile = Podfile.new do |p|
+      #       p.pod "Mixpanel"
+      #     end
+      #
+      #     should.raise(RuntimeError) {
+      #       transform_podfile(lockfile, original_podfile)
+      #     }
+      #   end
+      #
+      #   it "should transform to Podspec URL" do
+      #     lockfile = Lockfile.new({
+      #       "PODS" => ["Mixpanel (1.0)"]
+      #     })
+      #     original_podfile = Podfile.new do |p|
+      #       p.pod "Mixpanel"
+      #     end
+      #
+      #     podfile = transform_podfile(lockfile, original_podfile)
+      #     dependency = Dependency.new("Mixpanel", {:podspec => "https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/Mixpanel/1.0/Mixpanel.podspec.json"})
+      #     podfile.dependencies.should.include dependency
+      #   end
+      #
+      #   describe "which is a subspec" do
+      #     it "should transform to Root Podspec URL" do
+      #       lockfile = Lockfile.new({
+      #         "PODS" => ["Google/Analytics (1.0)"]
+      #       })
+      #       original_podfile = Podfile.new do |p|
+      #         p.pod "Google/Analytics"
+      #       end
+      #
+      #       podfile = transform_podfile(lockfile, original_podfile)
+      #       dependency = Dependency.new("Google/Analytics", {:podspec => "https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/Google/1.0/Google.podspec.json"})
+      #       podfile.dependencies.should.include dependency
+      #     end
+      #   end
+      # end
+      # it
+      #
+      # lockfile = Lockfile.new({
+      #   "PODS" => ["Mixpanel (1.0)"]
+      # })
+      # original_podfile = Podfile.new do |p|
+      #   p.pod "Mixpanel"
+      # end
+      #
+      # podfile = transform_podfile(lockfile, original_podfile)
+      # dependency = Dependency.new("Mixpanel", {:podspec => "https://raw.githubusercontent.com/CocoaPods/Specs/master/Specs/Mixpanel/1.0/Mixpanel.podspec.json"})
+      # podfile.dependencies.should.include dependency
     end
   end
 end
