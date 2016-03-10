63da671f8a6cd18891d500f0e54fc818fa75b561
James Campbell <james@supmenow.com>
2016-03-10 12:05:07 +0000

    Fixes to use Bacon syntax.

--- spec/command/deploy_spec.rb
+++ spec/command/deploy_spec.rb
@@ -3,28 +3,29 @@ require File.expand_path('../../spec_helper', __FILE__)
 module Pod
   describe Command::Deploy do
 
+    before do
+      @command = Command.parse(%w{ deploy })
+    end
+
     describe 'CLAide' do
       it 'registers it self' do
-        Command.parse(%w{ deploy }).should.be.instance_of Command::Deploy
+        @command.should.be.instance_of Command::Deploy
       end
     end
 
     it 'should disable cocoapods-stats' do
-      command = Command.parse(%w{deploy})
       ENV.expects(:[]=).with("COCOAPODS_DISABLE_STATS", "1")
-      command.run
+      @command.run
     end
 
     it 'should skip repo update' do
-      command = Command.parse(%w{deploy})
       Config.instance.expects(:skip_repo_update=).with(true)
-      command.run
+      @command.run
     end
 
     it 'should skip source file clean' do
-      command = Command.parse(%w{deploy})
       Config.instance.expects(:clean=).with(false)
-      command.run
+      @command.run
     end
   end
 end
