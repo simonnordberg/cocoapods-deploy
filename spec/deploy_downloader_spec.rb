54600887f9d6d017237e7de175f378c55a088d8c
James Campbell <james@supmenow.com>
2016-03-15 11:24:01 +0000

    Two specs.

--- spec/deploy_downloader_spec.rb
+++ spec/deploy_downloader_spec.rb
@@ -2,5 +2,12 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod
   describe DeployDownloader do
+    it "should download source from main repo" do
+
+    end
+
+    it "should download source from external repo" do
+
+    end
   end
 end
